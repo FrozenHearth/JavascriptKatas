e410fcd48ae3e8acb28d041eac0bf74ce6e8a67d
Vishu <brs@Vishus-MacBook-Air.local>
2020-03-13 11:57:43 +0530

    Fixes #5

--- kata2.js
+++ kata2.js
@@ -1,14 +1,16 @@
 const scale = (str, k, v) => {
   if (str === '') {
     return '';
-  } else {
-    const string = str
-      .split('')
-      .map(el => el.repeat(k)) // Iterate over the string and repeat each character k times (horizontal scaling)
-      .join('')
-      .split('\n') // Split the string into substring of group of characters before \n
-      .map(el => el.repeat(v)) // Iterate over the horizontally scaled string and repeat it v times (vertical scaling)
-      .join('\n');
-    return string;
   }
+  const string = str
+    .split('')
+    .map(item => item.repeat(k))
+    // Map over the string and repeat each character k times (horizontal scaling)
+    .join('')
+    .split('\n') // Split the string into substring of group of characters before \n
+    .filter(Boolean) // Remove empty strings
+    .map(item => item.concat('\n').repeat(v))
+    // Map over the horizontally scaled string, add \n after each substring and repeat it v times (vertical scaling)
+    .join('');
+  return string;
 };
