00bd4b802b0d91dfcf176f55434197f04612d25f
Vishwanath Sastry <FrozenHearth@users.noreply.github.com>
2020-03-16 11:46:11 +0530

    Update kata6.js

--- kata6.js
+++ kata6.js
@@ -1,9 +1,9 @@
 const ipToInt32 = ip => {
   let binaryIP = ip
     .split('.') // Break the string into comma separated substrings
-    .map(el => parseInt(el).toString(2)) // Iterate over the substrings, convert into a decimal value
+    .map(el => parseInt(el).toString(2)) // Iterate over the substrings, convert it into a decimal value
     .map(el => '00000000'.substring(el.length) + el) // Pad the substrings with leading 0s to represent it as an octet value
-    .join(''); // Join the substring and return as a single string
+    .join(''); // Join the substrings and return them as a single string
 
   return parseInt(binaryIP, 2); // Convert the binary representation into a 32-bit integer
 };
