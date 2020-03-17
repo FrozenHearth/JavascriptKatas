6600bdeae31a002f47db17d4afacdcf9fcb1e085
Vishu <brs@Vishus-MacBook-Air.local>
2020-03-17 11:57:17 +0530

    Partially solved kata #8

--- kata8.js
+++ kata8.js
@@ -1,1 +1,18 @@
+const findLongest = str => {
+  let count = 0;
+  let arr = str.split('');
+  let stack = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '(') {
+      stack.push('(');
+    }
+    for (let j = 0; j < stack.length; j++) {
+      if (arr[i] === ')' && stack[j] === '(') {
+        count = count + 2;
+        stack.pop();
+      }
+    }
+  }
 
+  return count;
+};
